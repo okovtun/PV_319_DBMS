cf8963b2d64f3978662fb5e9f1bcedad460b8f09
User <User@DESKTOP-0TCDSR6>
2025-03-06 19:17:41 +0300

    GetEasterDate DONE

--- SQL/Functions/SQLQuery0 - Check functions.sql
+++ SQL/Functions/SQLQuery0 - Check functions.sql
@@ -3,13 +3,14 @@ USE PV_319_Import;
 SET DATEFIRST 1;
 GO
 
-PRINT(dbo.GetGroupID(N'PV_319'));
-PRINT(dbo.GetDisciplineID(N'Hardware%'));
---EXEC sp_PrintScheduleForGroup N'PV_319';
-PRINT(dbo.CountSpentLessons(N'PV_319', N'Процедурное программирование%'));
-PRINT(dbo.GetNewYearHolidaysStartDate(2023));
-PRINT(dbo.GetSummerHolidaysStart(2023));
-PRINT(dbo.GetHolidaysDuration(N'Летние%'));
+--PRINT(dbo.GetGroupID(N'PV_319'));
+--PRINT(dbo.GetDisciplineID(N'Hardware%'));
+----EXEC sp_PrintScheduleForGroup N'PV_319';
+--PRINT(dbo.CountSpentLessons(N'PV_319', N'Процедурное программирование%'));
+--PRINT(dbo.GetNewYearHolidaysStartDate(2023));
+--PRINT(dbo.GetSummerHolidaysStart(2023));
+--PRINT(dbo.GetHolidaysDuration(N'Летние%'));
 
-PRINT('---------------------------');
-PRINT(dbo.GetHolidayStartDate(N'Летние%', 2025));
+--PRINT('---------------------------');
+--PRINT(dbo.GetHolidayStartDate(N'Летние%', 2025));
+PRINT(dbo.GetEasterDate(2004));
